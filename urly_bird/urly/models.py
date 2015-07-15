fed4d72778811c289de5deb8c607a38136db92bf
PJ Passalacqua <pjpassa@gmail.com>
2015-07-15 13:36:02 -0400

    basic models built

--- urly_bird/urly/models.py
+++ urly_bird/urly/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User)
+    url = models.URLField()
+    shortened_url = models.CharField(max_length=50)
+    created = models.TimeField(auto_now=True)
+
+
+class History(models.Model):
+    user = models.ForeignKey(User)
+    bookmark = models.ForeignKey(Bookmark)
+    created = models.TimeField(auto_now=True)
